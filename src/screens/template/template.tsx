17f619fde289e78f15b082365135a68f11a1fc8c
Olha Pavlenko <olgaigpavlenko@gmail.com>
2022-04-19 12:51:31 +0300

    feature/login corrected started screen

--- src/screens/template/template.tsx
+++ src/screens/template/template.tsx
@@ -1,43 +1,22 @@
-import React, { useEffect, useRef } from 'react';
-import { Animated, Image } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
 
-import { Icon } from '@components/icon';
 import { Screen } from '@components/screen';
-
-import { IMAGES } from '@constants/images';
-
 import { TemplateStyles } from './template.styles';
 
-const TIME = 1000;
-
 export const Template = () => {
-  const scale = useRef(new Animated.Value(0.6)).current;
-
-  useEffect(() => {
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(scale, {
-          toValue: 1,
-          useNativeDriver: true,
-          duration: TIME,
-        }),
-        Animated.timing(scale, {
-          toValue: 0.6,
-          useNativeDriver: true,
-          duration: TIME,
-        }),
-      ])
-    ).start();
-  }, [scale]);
-
   return (
-    <Screen isCentered background="white">
+    <Screen isCentered background="black">
       <TemplateStyles.ImageWrapper>
-        <Image source={IMAGES.codempire} />
+        <Text style={styles.white}>Codempire</Text>
+        <Text style={styles.white}>Cafe</Text>
       </TemplateStyles.ImageWrapper>
-      <Animated.View style={{ transform: [{ scale }] }}>
-        <Icon type="heart" iconColor="red" size={30} />
-      </Animated.View>
     </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  white: {
+    color: 'white',
+  },
+});
